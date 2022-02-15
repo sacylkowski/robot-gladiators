256b08c3fca8ae9b1d0d0ad9f11db66250f60d12
Sacylkowski <sacylkowski@Sarahs-MacBook-Pro.local>
2022-02-15 10:58:55 -0600

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -74,15 +74,26 @@ var fight = function (enemyName) {
         }
     }
 };
-       
-       
-    else {
-        window.alert("You need to choose a valid option. Try again!");
-    }
 
-
+// fight each enemy-robot by looping over them and fighting them one at a time
 for (var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    //if player is still alive, keeping fighting
+    if (playerHealth > 0) {
+        // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
+
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+
+        // reset enemyHealth befoer starting new fight
+        enemyHealth = 50;
+
+        // use debugger to pause script from running and check what's going on
+        // debugger;
+
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+    } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+    }
 }
